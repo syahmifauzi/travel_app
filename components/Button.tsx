d7d80129132e7fe01bfdb3a0e3f0601d59cf0ce8
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2023-10-14 09:33:22 +0800

    initial commit

---
+++ components/Button.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+type ButtonProp = {
+  type: 'button' | 'submit';
+  title: string;
+  icon?: string;
+  variant:
+    | 'btn_dark_green'
+    | 'btn_green'
+    | 'btn_white_text'
+    | 'btn_white'
+    | 'btn_dark_green_outline';
+  full?: boolean;
+};
+
+const Button = ({ type, title, icon, variant, full }: ButtonProp) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && 'w-full'
+      }`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
+};
+
+export default Button;
